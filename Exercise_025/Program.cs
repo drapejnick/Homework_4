89fe79ffc6672cb54003f94add047a265a46e773
Vladimir <drapejnick@gmail.com>
2022-09-13 19:05:11 +0300

    add exercise 025

---
+++ Exercise_025/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+int sum = a;
+
+for (int i = 1; i < b; i++)
+
+{
+    sum = sum * a;
+}
+
+Console.Write($"А в степени B равно {sum} ");
+
